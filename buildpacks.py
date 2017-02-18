33ff86738ef8473ffbc3c8fc3970945dd435f999
ToraenN <toraennakure@hotmail.com>
2017-02-18 09:34:30 -0500

    Fixing the bugfix
    The format of the rating tags make this necessary...

--- buildpacks.py
+++ buildpacks.py
@@ -150,13 +150,15 @@ def find_template_code(page):
 def id_gametypes(page):
     types = ['AB','FA','JQ','GvG','HA','RA','PvP team','general','farming','running','hero','SC','PvE team','CM']
     rawtypes = re.findall('<div class="build-types">(.*?)</div>', page, re.DOTALL)
+    gametypes = []
     if len(rawtypes) == 0:
-        gametypes = ['Uncategorized']
+        pass
     else:
-        gametypes = []
         for t in types:
             if rawtypes[0].find(t) > -1:
                 gametypes += [t]
+    if len(gametypes) == 0:
+        gametypes = ['Uncategorized']
     return gametypes
 
 def id_ratings(page): 
