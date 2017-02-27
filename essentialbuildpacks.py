b4a311185d51b49229ab61d1cdb7eeca590d5df8
Toraen <toraennakure@hotmail.com>
2017-02-27 05:30:48 -0500

    Trimming

--- essentialbuildpacks.py
+++ essentialbuildpacks.py
@@ -9,18 +9,16 @@ import urllib
 conn = httplib.HTTPConnection('gwpvx.gamepedia.com')
 
 def main():
-    print
+    global conn
     conn.request('GET', '/PvX_wiki')
     r1 = conn.getresponse()
     conn.close()
     if r1.status == 200:
         print "Holy shit! Curse is actually working. Now let's start getting that build data."
-        start_package()
     else:
-        httpfaildebugger('Start', r1.status, r1.response, r1.getheaders())
-
-def start_package():
-    global conn
+        print "Curse's servers are (probably) down. Try again later."
+        raise SystemExit()
+    
     # Unfortunately PvXwiki lacks a unified working builds category
     CATEGORIES = ['All_working_PvP_builds', 'All_working_PvE_builds']
     
@@ -42,83 +40,82 @@ def start_package():
             buildlist = category_page_list(page, buildlist)
             print "Builds from " + catname.replace('_',' ') + " added to list!"
         else:
-            httpfaildebugger(cat, response.status, response.reason, response.getheaders())
             print "Build listing for " + catname.replace('_',' ') + " failed."
     print str(len(buildlist)) + ' builds found!'
-    get_builds_and_write(buildlist, parameters)
+    get_builds_and_write(buildlist)
     print "Script complete."
     
 def get_builds_and_write(pagelist):
     for i in pagelist:
         # Check to see if the build has an empty primary profession (would generate an invalid template code)
+        buildname = urllib.unquote(i)
         if i.find('Any/') > -1:
-            print i + " skipped (empty primary profession)."
-        else:
-            print "Attempting " + (urllib.unquote(i)).replace('_',' ') + "..."
-            conn.request('GET', '/' + i.replace(' ','_').replace('\'','%27').replace('"','%22'))
-            response = conn.getresponse()
-            page = response.read()
-            conn.close()
-            if response.status == 200:
-                # Grab the build info
-                gametypes = id_gametypes(page)
-                ratings = id_ratings(page)
-                codes = find_template_code(page)
-                # If no template codes found on the build page, skip the build
-                if len(codes) == 0:
-                    print 'No template code found for ' + i + '. Skipped.'
-                    continue
-                # Establish directories
-                directories = []
-                for typ in gametypes:
-                    if len(typ) > 3 and typ.find('team') == -1:
-                        typdir = './PvX Build Packs/' + typ.title()
-                    else:
-                        typdir = './PvX Build Packs/' + typ
-                    if not os.path.isdir(typdir):
-                        os.mkdir(typdir)
-                    for rat in ratings:
-                        directories += [typdir + '/' + rat]
-                for d in directories:
-                    if not os.path.isdir(d):
-                        os.mkdir(d)
-                # Check to see if the build is a team build
-                if i.find('Team') >= 1 and len(codes) > 1:
-                    num = 0
-                    for j in codes:
-                        num += 1
-                        for d in directories:
-                            #Adds the team folder
-                            teamdir = file_name_sub(i, d)
-                            if not os.path.isdir(teamdir):
-                                os.mkdir(teamdir)
-                            outfile = open(file_name_sub(i, teamdir) + ' - ' + str(num) + '.txt','wb')
-                            outfile.write(j)
+            print buildname + " skipped (empty primary profession)."
+            continue
+        print "Attempting " + buildname + "..."
+        conn.request('GET', '/' + i.replace(' ','_').replace("'",'%27').replace('"','%22'))
+        response = conn.getresponse()
+        page = response.read()
+        conn.close()
+        if response.status == 200:
+            # Grab the build info
+            gametypes = id_gametypes(page)
+            ratings = id_ratings(page)
+            codes = find_template_code(page)
+            # If no template codes found on the build page, skip the build
+            if len(codes) == 0:
+                print 'No template code found for ' + buildname + '. Skipped.'
+                continue
+            # Establish directories
+            directories = []
+            for typ in gametypes:
+                if len(typ) > 3 and typ.find('team') == -1:
+                    typdir = './PvX Build Packs/' + typ.title()
                 else:
+                    typdir = './PvX Build Packs/' + typ
+                if not os.path.isdir(typdir):
+                    os.mkdir(typdir)
+                for rat in ratings:
+                    directories += [typdir + '/' + rat]
+            for d in directories:
+                if not os.path.isdir(d):
+                    os.mkdir(d)
+            # Check to see if the build is a team build
+            if i.find('Team') >= 1 and len(codes) > 1:
+                num = 0
+                for j in codes:
+                    num += 1
                     for d in directories:
-                        # Check for a non-team build with both player and hero versions, and sort them appropriately
-                        if len(codes) > 1 and ('hero' in gametypes) and ('general' in gametypes) and d.find('Hero') > -1:
-                            outfile = open(file_name_sub(i, d) + ' - Hero.txt','wb')
-                            outfile.write(codes[1])
-                        else:
-                            outfile = open(file_name_sub(i, d) + '.txt','wb')
-                            outfile.write(codes[0])
-                print i + " complete."
-            elif response.status == 301:
-                # Inserts the redirected build name into the pagelist array so it is done next
-                headers = str(response.getheaders())
-                newpagestr = re.findall("gwpvx.gamepedia.com/.*?'\)", headers)
-                newpagename = newpagestr[0].replace('gwpvx.gamepedia.com/','').replace("')",'').replace('_',' ')
-                newpagepos = pagelist.index(i) + 1
-                pagelist.insert(newpagepos, newpagename)
-                print '301 redirection...'
+                        #Adds the team folder
+                        teamdir = file_name_sub(i, d)
+                        if not os.path.isdir(teamdir):
+                            os.mkdir(teamdir)
+                        outfile = open(file_name_sub(buildname, teamdir) + ' - ' + str(num) + '.txt','wb')
+                        outfile.write(j)
             else:
-                httpfaildebugger(i, response.status, response.reason, response.getheaders())
-                print i + " failed."
+                for d in directories:
+                    # Check for a non-team build with both player and hero versions, and sort them appropriately
+                    if len(codes) > 1 and ('hero' in gametypes) and ('general' in gametypes) and d.find('Hero') > -1:
+                        outfile = open(file_name_sub(buildname, d) + ' - Hero.txt','wb')
+                        outfile.write(codes[1])
+                    else:
+                        outfile = open(file_name_sub(buildname, d) + '.txt','wb')
+                        outfile.write(codes[0])
+            print buildname + " complete."
+        elif response.status == 301:
+            # Inserts the redirected build name into the pagelist array so it is done next
+            headers = str(response.getheaders())
+            newpagestr = re.findall("gwpvx.gamepedia.com/(.*?)'\)", headers)
+            newpagename = newpagestr[0].replace('_',' ')
+            newpagepos = pagelist.index(i) + 1
+            pagelist.insert(newpagepos, newpagename)
+            print '301 redirection...'
+        else:
+            print i + " failed."
 
 def file_name_sub(build, directory):
     #Handles required substitutions for build filenames
-    filename = directory + '/' + (urllib.unquote(build)).replace('Build:','').replace('Archive:','').replace('/','_').replace('"','\'\'')
+    filename = directory + '/' + build.replace('Build:','').replace('Archive:','').replace('/','_').replace('"','\'\'')
     return filename
 
 def category_page_list(page, newlist):
@@ -163,24 +160,6 @@ def id_ratings(page):
     elif page.find('in the range from 3.75') > -1:
         ratings += ['Good']
     return ratings
-
-def httpfaildebugger(attempt, response, reason, headers):
-    debuglog = open('./buildpackshttpdebug.txt', 'ab')
-    debuglog.write(str(attempt) + '\r\n' + str(response) + ' - ' + str(reason) + '\r\n' + str(headers) + '\r\n' + '----' + '\r\n')
-    print 'HTTPConnection error encountered: ' + str(response) + ' - ' + str(reason)
-    if attempt == 'Start':
-        print "Curse's servers are (probably) down. Try again later."
-        raise SystemExit()
-    answer = ''
-    while not answer == ('y' or 'n'):
-        answer = raw_input('Do you wish to continue the script? ' + str(attempt) + ' will be skipped. (y/n) ')
-        if answer == 'y':
-            print 'Ok, continuing...'
-        elif answer == 'n':
-            print 'Ok, exiting...'
-            raise SystemExit()
-        else:
-            print 'Please enter \'y\' or \'n\'.'
 
 if __name__ == "__main__":
     main()
