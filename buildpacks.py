3fc8f24cd076ccb17cec82a476bf49c77107e71c
Toraen <toraennakure@hotmail.com>
2017-06-12 07:03:16 -0400

    format_time() & build_error()
    +readability (replacing all the .find('') > -1 syntax with in statements)

--- buildpacks.py
+++ buildpacks.py
@@ -8,29 +8,29 @@ import time
 import urllib.request, urllib.parse, urllib.error
 import zipfile
 
-conn = http.client.HTTPConnection('gwpvx.gamepedia.com')
-parameters = input('Parameters (h for help): ')
-while parameters.find('h') > -1:
-    print('a: don\'t save Any/X builds.')
-    print('b: block consolidated packs explicitly.')
-    print('c: list and choose from preset categories.')
-    print('o: change folder layout.')
-    print('q: manual category entry. Enter as many categories as you want.')
-    print('s: silent mode.')
-    print('t: save text files even when saving zip files.')
-    print('w: write log.')
-    print('y: build consolidated packs even with category/sort selects (overrides "b")')
-    print('z: save as zip files.')
-    parameters = input('Parameters: ')
-
 def main():
     global conn
     global parameters
-    if parameters.find('w') > -1:
+    conn = http.client.HTTPConnection('gwpvx.gamepedia.com')
+    parameters = input('Parameters (h for help): ')
+    while 'h' in parameters:
+        print('a: don\'t save Any/X builds.')
+        print('b: block consolidated packs explicitly.')
+        print('c: list and choose from preset categories.')
+        print('o: change folder layout.')
+        print('q: manual category entry. Enter as many categories as you want.')
+        print('s: silent mode.')
+        print('t: save text files even when saving zip files.')
+        print('w: write log.')
+        print('y: build consolidated packs even with category/sort selects (overrides "b")')
+        print('z: save as zip files.')
+        parameters = input('Parameters: ')
+    if 'w' in parameters:
         global logname
-        datetime = time.gmtime()
-        logname = './buildpacklog ' + str(datetime[1]).zfill(2) + str(datetime[2]).zfill(2) + str(datetime[3]).zfill(2) + str(datetime[4]).zfill(2) + str(datetime[5]).zfill(2) + '.txt'
-        log_write('Parameters: ' + parameters + '\r\n')
+        datetime = format_time()
+        logname = './buildpacklog ' + datetime[2] + '.txt'
+        log_write(datetime[0])
+        log_write('Parameters: ' + parameters)
     conn.request('GET', '/PvX_wiki')
     r1 = conn.getresponse()
     conn.close()
@@ -39,7 +39,7 @@ def main():
     else:
         http_failure('Start', r1.status, r1.response, r1.getheaders())
     # If we're changing the sorts, call the function until the user inputs something valid. Otherwise default to gametype-only sort.
-    if parameters.find('o') > -1:
+    if 'o' in parameters:
         dirorder = False
         while dirorder == False:
             dirorder = change_dir_order()
@@ -47,12 +47,12 @@ def main():
         dirorder = 'g'
     # Check for category selection modes. If none of these, just grab the tested builds.
     categories = []
-    if parameters.find('q') > -1:
+    if 'q' in parameters:
         manualcatentry = print_prompt('Enter category (leave blank to end entry): ')
         while manualcatentry != '':
             categories += [manualcatentry.replace(' ','_')]
             manualcatentry = print_prompt('Enter category (leave blank to end entry): ')
-    if parameters.find('c') > -1:
+    if 'c' in parameters:
         if 'y' in print_prompt('Do you want any PvP builds? (y/n) '):
             if 'y' in print_prompt('All of them? (y/n) '):
                 categories += ['All_working_PvP_builds']
@@ -100,7 +100,7 @@ def main():
 
 def get_build(i, dirorder):
     # Check to see if the build has an empty primary profession as that would generate an invalid template code in Guild Wars (but not in build editors)
-    if (i.find('Any/') > -1) and (parameters.find('a') > -1):
+    if 'Any/' in i and 'a' in parameters:
         print_log(i + " skipped (empty primary profession).")
         return
     print_log("Attempting " + (urllib.parse.unquote(i)).replace('_',' ') + "...")
@@ -113,21 +113,16 @@ def get_build(i, dirorder):
         codes = re.findall('<input id="gws_template_input" type="text" value="(.*?)"', page)
         # If no template codes found on the build page, prompt user to fix the page
         if len(codes) == 0:
-            print_log('No build template found on page for ' + i + '.', 'yes')
-            skip = print_prompt('Press enter to reattempt after fixing the build page. (type "s" to skip)')
-            if not re.search('s', skip) == None:
-                return i
-            else:
-                return
+            resolution = build_error('No build template found on page for ' + i + '.', 'ers', i)
+            return resolution
         # Some people don't remember to assign the secondary profession and this happens...
         for c in codes:
             if c == '':
-                print_log('Warning: Blank code found in ' + i + '! (code #: ' + code.index + ')', 'yes')
-                skip = print_prompt('Press enter to reattempt after fixing the build page. (type "s" to skip)')
-                if not re.search('s', skip) == None:
-                    return i
+                resolution = build_error('Warning: Blank code found in ' + i + '! (code #' + str(codes.index(c) + 1) + ')', 'ecrs', i)
+                if resolution == 'c':
+                    break
                 else:
-                    return
+                    return resolution
         # Grab all the other build info
         fluxes = id_fluxes(page)
         profession = id_profession(i)
@@ -145,18 +140,18 @@ def get_build(i, dirorder):
             elif o == 'r':
                 dirlevels += [ratings]
         # Determine if the rating needs to go in the build name
-        if dirorder.find('r') > -1:
+        if 'r' in dirorder:
             rateinname = ''
         else:
             rateinname = ' - ' + str(ratings).replace('[','').replace(']','').replace("'",'').replace(',','-').replace(' ','')
-        if i.find('Team') >= 1 and len(codes) > 1:
+        if 'Team' in i and len(codes) > 1:
             dirlevels += [[(file_name_sub(i,'') + rateinname)]]
         directories = directory_tree(dirlevels)
         # If we're making a log file, inlcude the build info
-        if parameters.find('w') > -1:
-            log_write('Fluxes found: ' + str(fluxes) + '\r\nProfession: ' + str(profession) + '\r\nGametypes found: ' + str(gametypes) + '\r\nRatings found: ' + str(ratings) + '\r\nCodes found: ' + str(codes) + '\r\nDirectories used: ' + str(directories) + '\r\n')
+        if 'w' in parameters:
+            log_write('Fluxes found: ' + str(fluxes) + '\r\nProfession: ' + str(profession) + '\r\nGametypes found: ' + str(gametypes) + '\r\nRatings found: ' + str(ratings) + '\r\nCodes found: ' + str(codes) + '\r\nDirectories used: ' + str(directories))
         # Check to see if the build is a team build
-        if i.find('Team') >= 1 and len(codes) > 1:
+        if 'Team' in i and len(codes) > 1:
             num = 0
             for j in codes:
                 num += 1
@@ -165,11 +160,11 @@ def get_build(i, dirorder):
         else:
             for d in directories:
                 # Check for a non-team build with both player and hero versions, and sort them appropriately
-                if (len(codes) > 1) and ('Hero' in gametypes) and ('General' in gametypes):
-                    if d.find('Hero') > -1:
+                if len(codes) > 1 and 'Hero' in gametypes and 'General' in gametypes:
+                    if 'Hero' in d:
                         write_build(file_name_sub(i, d) + ' - Hero' + rateinname + '.txt', codes[1])
                     # If we're not sorting by gametype, save both versions
-                    elif dirorder.find('g') == -1:
+                    elif not 'g' in dirorder:
                         write_build(file_name_sub(i, d) + ' - Hero' + rateinname + '.txt', codes[1])
                         write_build(file_name_sub(i, d) + rateinname + '.txt', codes[0])
                 else:
@@ -188,11 +183,11 @@ def get_build(i, dirorder):
 
 def write_build(filename, code):
     # Check if we're writing text files
-    if parameters.find('t') > -1 or parameters.find('z') == -1:
+    if 't' in parameters or not 'z' in parameters:
         with open(filename, 'w') as outfile:
             outfile.write(code)
     # Check if we're writing zip files
-    if parameters.find('z') > -1:
+    if 'z' in parameters:
         if not os.path.isdir('./Zipped Build Packs'):
             os.makedirs('./Zipped Build Packs')
         try:
@@ -203,7 +198,7 @@ def write_build(filename, code):
         archivename = filename.replace('./PvX Build Packs/','')
         zip_file_write(TopDir, archivename, code)
         # If there are any non-default sorts or limited categories in use, don't continue to the consolidated packs. Overridden by 'y'.
-        if re.search(r'[bcoq]', parameters) and not re.search('y', parameters):
+        if re.search(r'[bcoq]', parameters) and not 'y' in parameters:
             return
         zip_file_write('All Build Packs', archivename, code)
         if TopDir in ['HA','GvG','RA','AB','FA','JQ','PvP team','TA','CM','HB']:
@@ -262,11 +257,11 @@ def directory_tree(dirlevels):
         for e in dirlevels[4]:
          addeddir = './PvX Build Packs/' + a + '/' + b + '/' + c + '/' + d + '/' + e + '/'
          # '//' will mess up zip writing
-         while addeddir.find('//') > -1:
+         while '//' in addeddir:
              addeddir = addeddir.replace('//','/')
          directories += [addeddir]
     # Only create directories if saving text files
-    if (parameters.find('t') > -1) or (parameters.find('z') == -1):
+    if 't' in parameters or not 'z' in parameters:
         for folder in directories:
             if not os.path.isdir(folder):
                 os.makedirs(folder)
@@ -295,7 +290,7 @@ def id_gametypes(page):
     # Build the gametypes list based on the tags
     gametypes = []
     for t in rawtypes:
-        if t.find('team') > -1:
+        if 'team' in t:
             cleanedtype = re.sub('<br />', ' ', t)
         elif len(t) > 12:
             cleanedtype = (re.sub('Pv[EP]<br />', '', t)).title()
@@ -308,24 +303,24 @@ def id_gametypes(page):
 
 def id_ratings(page): 
     ratings = []
-    if page.find('This build is part of the current metagame.') > -1:
+    if 'This build is part of the current metagame.' in page:
         ratings += ['Meta']
     # A second if statement because builds can have both Meta and one of Good/Great
-    if page.find('in the range from 4.75') > -1:
+    if 'in the range from 4.75' in page:
         ratings += ['Great']
-    elif page.find('in the range from 3.75') > -1:
+    elif 'in the range from 3.75' in page:
         ratings += ['Good']
-    elif page.find('below 3.75') > -1:
+    elif 'below 3.75' in page:
         ratings += ['Trash']
     elif re.search(r'This build article is a <a.*?>stub</a>', page):
         ratings += ['Stub']
-    elif page.find('in the <i>trial</i> phase.') > -1:
+    elif 'in the <i>trial</i> phase.' in page:
         ratings += ['Trial']
-    elif page.find('This build is currently being tested.') > -1:
+    elif 'This build is currently being tested.' in page:
         ratings += ['Testing']
-    elif page.find('been archived') > -1:
+    elif 'been archived' in page:
         ratings += ['Archived']
-    elif page.find('File:Image_Abandoned.jpg') > -1:
+    elif 'File:Image_Abandoned.jpg' in page:
         ratings += ['Abandoned']
     if ratings == []:
         ratings = ['Nonrated']
@@ -333,31 +328,32 @@ def id_ratings(page):
 
 def print_prompt(string):
     answer = input(string)
-    if parameters.find('w') > -1:
-        log_write(string + answer + '\r\n')
+    if 'w' in parameters:
+        log_write(string + answer)
     return answer
 
 def print_log(string, alwaysdisplay = 'no'):
-    if (parameters.find('s') == -1) or (alwaysdisplay == 'yes'):
+    if not 's' in parameters or alwaysdisplay == 'yes':
         print(string)
-    if parameters.find('w') > -1:
-        log_write(str(string) + '\r\n')
+    if 'w' in parameters:
+        log_write(str(string))
 
 def log_write(string):
     with open(logname, 'a') as textlog:
-        textlog.write(string)
+        timestamp = format_time()
+        textlog.write(timestamp[1] + ': ' + string.replace('\r\n', '\r\n' + ' ' * 10) + '\r\n')
 
 def http_failure(attempt, response, reason, headers):
     print_log('HTTPConnection error encountered: ' + str(response) + ' - ' + str(reason), 'yes')
-    if parameters.find('w') > -1:
-        log_write('----\r\n' + str(attempt) + '\r\n' + str(response) + ' - ' + str(reason) + '\r\n' + str(headers) + '\r\n----\r\n')
+    if 'w' in parameters:
+        log_write('----\r\n' + str(attempt) + '\r\n' + str(response) + ' - ' + str(reason) + '\r\n' + str(headers) + '\r\n----')
     if attempt == 'Start':
         print_log("Curse's servers are (probably) down. Try again later.", 'yes')
         raise SystemExit()
     # Require a definitive answer from the user
-    answer = ''
-    while not answer == ('y' or 'n'):
-        answer = print_prompt('Do you wish to continue the script? ' + str(attempt) + ' will be skipped. (y/n) ')
+    answer = print_prompt('Do you wish to continue the script? ' + str(attempt) + ' will be skipped. (y/n) ')
+    while not re.search('^[ny]$',answer):
+        answer = print_prompt('Please enter "y" or "n".')
         if answer == 'y':
             print_log('Ok, continuing...', 'yes')
         elif answer == 'n':
@@ -365,6 +361,37 @@ def http_failure(attempt, response, reason, headers):
             raise SystemExit()
         else:
             print_log("Please enter 'y' or 'n'.", 'yes')
+
+def format_time():
+    datetime = time.gmtime()
+    curdate = str(datetime[0]) + '.' + str(datetime[1]).zfill(2) + '.' + str(datetime[2]).zfill(2)
+    curtime = str(datetime[3]).zfill(2) + ':' + str(datetime[4]).zfill(2) + ':' + str(datetime[5]).zfill(2)
+    filesuffix = str(datetime[1]).zfill(2) + str(datetime[2]).zfill(2) + str(datetime[3]).zfill(2) + str(datetime[4]).zfill(2) + str(datetime[5]).zfill(2)
+    return (curdate, curtime, filesuffix)
+
+def build_error(error, options, build):
+    print_log(error, 'yes')
+    resprompt = 'Please choose one of the following options:\r\n'
+    if 'c' in options:
+        resprompt += 'c = continue the build (with errors)\r\n'
+    if 'e' in options:
+        resprompt += 'e = exit the script\r\n'
+    if 'r' in options:
+        resprompt += 'r = reattempt the build\r\n'
+    if 's' in options:
+        resprompt += 's = skip the build\r\n'
+    resprompt += 'Type the letter corresponding to your choice: '
+    resolution = print_prompt(resprompt)
+    while re.search('^[' + options +']$', resolution) == None:
+        resolution = print_prompt('Please enter a valid option: ')
+    if resolution == 'c':
+        return resolution
+    elif resolution == 'e':
+        raise SystemExit()
+    elif resolution == 'r':
+        return build
+    elif resolution == 's':
+        return None
 
 if __name__ == "__main__":
     main()
